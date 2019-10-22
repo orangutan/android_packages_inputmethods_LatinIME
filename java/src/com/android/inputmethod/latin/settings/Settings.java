70d4c1d510617f9ef07749730d25768b289de54a
orangutan <anonymousid@veryprivate.com>
2019-10-22 15:13:05 +0530

    show app icon 2
    Change-Id: I27905dece21feff75613401f352f5dcc0e0fbaaf

--- java/src/com/android/inputmethod/latin/settings/Settings.java
+++ java/src/com/android/inputmethod/latin/settings/Settings.java
@@ -361,11 +361,15 @@ public final class Settings implements SharedPreferences.OnSharedPreferenceChang
     public static boolean readShowSetupWizardIcon(final SharedPreferences prefs,
             final Context context) {
         if (!prefs.contains(PREF_SHOW_SETUP_WIZARD_ICON)) {
-            final ApplicationInfo appInfo = context.getApplicationInfo();
-            final boolean isApplicationInSystemImage =
-                    (appInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0;
+            //final ApplicationInfo appInfo = context.getApplicationInfo();
+            //final boolean isApplicationInSystemImage =
+                    //(appInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0;
             // Default value
-            return !isApplicationInSystemImage;
+            //return !isApplicationInSystemImage;
+		// pref not found, lets create and set to true
+		final SharedPreferences.Editor editor = prefs.edit();
+        	editor.putBoolean(PREF_SHOW_SETUP_WIZARD_ICON, true);
+        	editor.apply();
         }
         return prefs.getBoolean(PREF_SHOW_SETUP_WIZARD_ICON, true);
     }
